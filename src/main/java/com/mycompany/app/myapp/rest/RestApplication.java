635b1b8ac0e914d5fce927effa21febf7f55f4e2
apu4711 <heiko@kallisti.de>
2018-09-03 13:57:19 +0200

    Java-Projext and more

---
+++ src/main/java/com/mycompany/app/myapp/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package com.mycompany.app.myapp.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
